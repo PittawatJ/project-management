3a4f99573602df3a693578702974290cd7cc320e
Chock .P <pittawat.j@takeitsolution.com>
2024-10-29 17:11:33 +0700

    first commit

---
+++ addons/project_install/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    "name": "Odoo Project Install",
+    "version": "17.0.1.0.0",
+    "summary": "",
+    "sequence": 10,
+    "author": "Takeitsolution Co.,Ltd.",
+    "category": "Extra Tools",
+    "description": """""",
+    "website": "",
+    "depends": [
+        # Core Odoo
+        "base",
+        "web",
+        "project",
+        # OCA
+        "web_responsive",
+        "web_refresher",
+    ],
+    "external_dependencies": {},
+    "data": [],
+    "demo": [],
+    "qweb": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "post_init_hook": "",
+    "license": "AGPL-3",
+}
